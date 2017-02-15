68ac661d42bd37292a9ad318bc222f70b0e359c7
u4bi <u4bi@u4bi.com>
2017-02-15 10:29:06 +0900

    깃헙 프로필 페이지 스크래핑 - 레포 수 - 스타 수 - 팔로워 수 - 팔로잉 수

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var target = 'http://u4bi.com';
+var target = 'https://github.com/u4bi';
 
 request(target, function(err, response, body){
-    if(!err && response.statusCode == 200) console.log(body);
+    if(!err && response.statusCode == 200){
+        $ = cheerio.load(body);
+        
+        $('.user-profile-nav a .counter').each(function(index, title){
+            console.log($(title).text());
+        });
+    }
 });
